aaefed0db80cf65e163526d3f663803307ffef1c
Gabor Racz <orfeous.b@gmail.com>
2020-09-01 17:02:02 +0100

    Initial push

---
+++ DummyApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DummyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                foreach (string arg in args)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
